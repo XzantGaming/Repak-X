60e6e113125a254770e89e1f72a6648668e6499e
NikolasBecic <b.nikolas.contact@gmail.com>
2025-12-04 13:47:07 +0100

    Removing useless Files

--- START_SENDER.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Repak GUI - SENDER
-echo ========================================
-echo Starting SENDER Instance
-echo ========================================
-echo.
-cd repak-gui
-cargo tauri dev
